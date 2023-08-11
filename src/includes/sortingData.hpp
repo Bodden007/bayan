3dfadd632d9cbd0412e4892fd23d58e589070db8
Bodden007 <denis-bodrov@mail.ru>
2023-08-11 18:09:53 +0500

    Found double

--- src/includes/sortingData.hpp
+++ src/includes/sortingData.hpp
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <boost/algorithm/cxx11/any_of.hpp>
 
 class sortingData
 {
 private:
-    /* data */
+    int quanKey;
+    std::vector<std::string> keyMap;
+    std::vector<std::string> comparMap;
+    std::vector<std::string> elementMap;
 public:
     void sortDat(std::map<std::string, std::vector<std::string>> dataMap);
 };
-
-
--- src/models/sortingData.cpp
+++ src/models/sortingData.cpp
@@ -5,11 +5,32 @@ void sortingData::sortDat(std::map<std::string, std::vector<std::string>> dataMa
     for (const auto &[key, vec] : dataMap)
     {
         std::cout << std::endl;
-        std::cout << "-------Key:  " << key << std::endl;
-
-        for (const auto element : vec)
-            std::cout << "value:  " << element << std::endl;
+        keyMap.push_back(key);
+        // std::cout << "dataMap Key:  " << key << std::endl;
     }
-
+    quanKey = dataMap.size();
     std::cout << "Size:  " << dataMap.size() << std::endl;
+
+    for (int i = 0; i < quanKey - 1; i++)
+    {
+        comparMap = dataMap.at(keyMap[i]);
+        for (int j = i + 1; j < quanKey; j++)
+        {
+            elementMap = dataMap.at(keyMap[j]);
+
+            for (int p = 0; p < comparMap.size(); p++)
+            {
+                bool found = boost::algorithm::any_of_equal(elementMap, comparMap[p]);
+                if (found)
+                {
+                    std::cout << "Matches found in the directory:" << std::endl;
+                    std::cout << "First catalog: " << keyMap[i] << std::endl;
+                    std::cout << "Second catalog: " << keyMap[j] << std::endl;
+                    std::cout << "file: " << comparMap[i] << std::endl;
+                }
+            }
+            elementMap.clear();
+        }
+        comparMap.clear();
+    }
 }
